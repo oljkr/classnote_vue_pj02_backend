0304d7fcbdd0bdf284f70882dc086eaf0a01635c
Eunju Jeong <zaoljkr@gmail.com>
2023-01-03 15:29:49 +0900

    vue.js와 서버통신

---
+++ src/main/java/kr/co/itwill/memo/MemoCont.java
@@ -0,0 +1,32 @@
+package kr.co.itwill.memo;
+
+import org.json.simple.JSONObject;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MemoCont {
+	public MemoCont() {
+		System.out.println("-----MemeCont() 객체 생성됨");
+	}//end
+	
+	@RequestMapping("/memolist")
+	public JSONObject[] memolist() {
+		 JSONObject dto1=new JSONObject();
+	        dto1.put("memono", 1);
+	        dto1.put("subject", "무궁화 꽃이 피었습니다");
+	        dto1.put("writer", "오필승");
+	        dto1.put("readcnt", 5);
+	        
+	        JSONObject dto2=new JSONObject();
+	        dto2.put("memono", 2);
+	        dto2.put("subject", "바람과 함께 사라지다");
+	        dto2.put("writer", "코리아");
+	        dto2.put("readcnt", 7);
+	        
+	        JSONObject[] jsons= {dto1,dto2};
+	        return jsons;
+		
+	}//memolist
+
+}//class end
